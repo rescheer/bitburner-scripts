c3e29cd68fbd21408ad313c4c1c39f872baf2593
Robby Scheer <robertedwinscheer@gmail.com>
2023-11-25 01:58:54 -0600

    feat(hacking-mgr): respect home ram reservation

--- managers/hacking-mgr.js
+++ managers/hacking-mgr.js
@@ -1,18 +1,30 @@
-/** @param {NS} ns */
 import { gameConfig, portConfig } from 'config.js';
-import { peekPortObject, readPortObject } from "lib/Ports.js";
+import { peekPortObject, readPortObject } from 'lib/Ports.js';
 
+/** @param {NS} ns */
 export async function main(ns) {
   const configPort = ns.getPortHandle(portConfig.config);
+  const networkMap = JSON.parse(ns.read(gameConfig.files.netmap));
   const newTargetsPort = ns.getPortHandle(portConfig.newDeployerTargets);
 
   while (true) {
     // sleep at end
     const playerSettings = peekPortObject(configPort);
+    const homeMaxRam = networkMap[gameConfig.home].maxRam;
+    const deployerRamUse = ns.getScriptRam(gameConfig.scripts.deployer);
+    const { reservedRamPercent, reservedRamGb } = playerSettings.home;
+    const homeUnreservedRam =
+      homeMaxRam - Math.max(reservedRamPercent * homeMaxRam, reservedRamGb);
+    var maxDeployerThreads = Math.floor(homeUnreservedRam / deployerRamUse);
+
     if (playerSettings.log.silenced) {
-      ns.disableLog('ALL');
+      if (ns.isLogEnabled('ALL')) {
+        ns.disableLog('ALL');
+      }
     } else {
-      ns.enableLog('ALL');
+      if (!ns.isLogEnabled('ALL')) {
+        ns.enableLog('ALL');
+      }
     }
 
     if (!newTargetsPort.empty()) {
@@ -20,13 +32,24 @@ export async function main(ns) {
       var newDeployers = 0;
 
       while (newTargetsData.length) {
-        const targetNode = newTargetsData.shift().node;
-        ns.run(gameConfig.scripts.deployer, 1, targetNode);
-        newDeployers += 1;
+        if (maxDeployerThreads > 0) {
+          const targetNode = newTargetsData.shift().node;
+          ns.run(gameConfig.scripts.deployer, 1, targetNode);
+          newDeployers += 1;
+          maxDeployerThreads -= 1;
+        }
       }
 
       if (newDeployers > 0) {
-        ns.tprint(`Started ${newDeployers} new deployers.`);
+        if (newTargetsData.length) {
+          ns.tprint(
+            `Started ${newDeployers}/${
+              newDeployers + newTargetsData.length
+            } new deployer(s) due to low RAM.`
+          );
+        } else {
+          ns.tprint(`Started ${newDeployers} new deployers.`);
+        }
       }
     }
 
