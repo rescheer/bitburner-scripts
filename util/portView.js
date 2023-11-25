49ae11454f7c0f5dd4d7e4ada4d06c0d19fa5780
Robby Scheer <robertedwinscheer@gmail.com>
2023-11-25 01:55:53 -0600

    fix(portView): display object in readable format

--- util/portView.js
+++ util/portView.js
@@ -6,7 +6,11 @@ export async function main(ns) {
     const portData = ns.peek(port);
 
     ns.tprint(`Port ${port} current data:`);
-    ns.tprint(`${portData}`);
+    if (typeof portData === 'object') {
+      ns.tprint(JSON.stringify(portData, null, 2));
+    } else {
+      ns.tprint(`${portData}`);
+    }
   } else {
     tprint(`Missing or invalid port number given.`);
   }
