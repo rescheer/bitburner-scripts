45f8e42514e98c82b8f42e7b51ef2debb04d0057
Robby Scheer <robertedwinscheer@gmail.com>
2023-11-27 12:36:17 -0600

    switch to airbnb lint rules, add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-constant-condition": "off"
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-constant-condition': 'off',
+    'import/prefer-default-export': 'off',
+    'no-await-in-loop': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
