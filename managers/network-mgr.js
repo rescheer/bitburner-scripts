0bde1b88e089cf3ffc52ef283557de4657ac938e
Robby Scheer <robertedwinscheer@gmail.com>
2024-07-21 10:45:25 -0700

    feat(network-mgr): add map-only flag

--- managers/network-mgr.js
+++ managers/network-mgr.js
@@ -4,20 +4,25 @@ import PortWrapper from 'lib/PortWrapper';
 /** @param {NS} ns */
 export async function main(ns) {
   const configPort = new PortWrapper(ns, portConfig.config);
+  const flags = ns.flags([['map-only', false]]);
 
-  while (true) {
-    const playerSettings = configPort.peek();
-    if (playerSettings.log.silenced) {
-      if (ns.isLogEnabled('sleep')) {
-        ns.disableLog('ALL');
+  if (!flags['map-only']) {
+    while (true) {
+      const playerSettings = configPort.peek();
+      if (playerSettings.log.silenced) {
+        if (ns.isLogEnabled('sleep')) {
+          ns.disableLog('ALL');
+        }
+      } else if (!ns.isLogEnabled('sleep')) {
+        ns.enableLog('ALL');
       }
-    } else if (!ns.isLogEnabled('sleep')) {
-      ns.enableLog('ALL');
+
+      ns.run(gameConfig.scripts.netmap);
+      ns.run(gameConfig.scripts.nuker);
+
+      await ns.sleep(playerSettings.netmap.interval);
     }
-
+  } else {
     ns.run(gameConfig.scripts.netmap);
-    ns.run(gameConfig.scripts.nuker);
-
-    await ns.sleep(playerSettings.netmap.interval);
   }
 }
